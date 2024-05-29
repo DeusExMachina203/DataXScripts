4969152cf0e3deb13a1122ee2959f4c374ed36d9
Ernesto de Loayza <ernestodeloayza@gmail.com>
2024-05-28 21:42:11 -0500

    agregado archivo con drops

---
+++ DropTablesQuery.sql
@@ -0,0 +1,27 @@
+drop table TipoVia;
+drop table Region;
+drop table Provincia;
+drop table Distrito;
+drop table Direccion;
+drop table Empresa;
+drop table CorreoEmpresa;
+drop table CodigoTelefonoPais;
+drop table TelefonoEmpresa;
+drop table Almacen;
+drop table Producto;
+drop table Inventario;
+drop table Cliente;
+drop table Negociador;
+drop table TelefonoCliente;
+drop table CorreoCliente;
+drop table SedeCliente;
+drop table TarjetaCredito;
+drop table TipoMembresia;
+drop table Membresia;
+drop table Pedido;
+drop table ResumenPedido;
+drop table FormaPago;
+drop table factura;
+
+
+--Dale a f5 varias veces para que se borre todo, no importa que te de error xd
