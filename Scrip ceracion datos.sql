d5a28fbb2c6860aa8966c0ac784006d1ed63c24b
Emma Ro <yselaerrm@gmail.com>
2024-05-31 23:39:53 -0500

    Cargar datos en Almacen

--- Scrip ceracion datos.sql
+++ Scrip ceracion datos.sql
@@ -189,6 +189,7 @@ INSERT INTO CodigoTelefonoPais (Codigo, NombrePais) VALUES
 ('+61', 'Australia'),
 ('+55', 'Brasil');
 
+--Cargar datos en TelefonoEmpresa
 INSERT INTO TelefonoEmpresa (Telefono, Descripcion, IdEmpresa, IdCodigoPais) VALUES 
 ('900000001', 'Atención al cliente', 1, 1),
 ('900000002', '', 2, 1),
@@ -216,11 +217,33 @@ INSERT INTO TelefonoEmpresa (Telefono, Descripcion, IdEmpresa, IdCodigoPais) VAL
 ('900000024', 'Atención de pedidos', 24, 1),
 ('900000025', 'Línea de contacto', 25, 1);
 
-
-
-
-
-
+-- Cargar datos en Almacen
+INSERT INTO Almacen (IdDireccion, IdEmpresa) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11,11),
+(12,12),
+(13,13),
+(14,14),
+(15,15),
+(16,16),
+(17,17),
+(18,18),
+(19,19),
+(20,20),
+(21,21),
+(22,22),
+(23,23),
+(24,24),
+(25,25);
 
 
 
